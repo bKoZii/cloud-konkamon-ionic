09e48b58ce5e932a81fd6d3f88fb19d1cd20bb74
bKoZii <bigkonkamon@gmail.com>
2024-03-11 11:42:47 +0700

    Added Firebase Service

---
+++ src/app/services/member.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, OnInit } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+import { IMember } from '../models/member.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+  dbPath = 'members';
+
+  memberRef: AngularFirestoreCollection<IMember>;
+  constructor(private db: AngularFirestore) {
+    this.memberRef = db.collection(this.dbPath);
+  }
+
+  submitData(data: IMember): any {
+    // console.log(data);
+    return this.memberRef.add({ ...data });
+  }
+
+  retrieveData(): AngularFirestoreCollection<IMember> {
+    return this.memberRef;
+  }
+  updateData(id: string, data: any): Promise<void> {
+    return this.memberRef.doc(id).update(data);
+  }
+
+  deleteData(id: any): Promise<void> {
+    return this.memberRef.doc(id).delete();
+  }
+}
