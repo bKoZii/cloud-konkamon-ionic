5528a6e904af68e23ae949028769319250cd0c22
Konkamon Sion <bigkonkamon@gmail.com>
2024-03-18 13:19:44 +0700

    Feat: Updated Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,31 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
   {
     path: 'project',
-    loadChildren: () => import('./project/project.module').then( m => m.ProjectPageModule)
+    loadChildren: () =>
+      import('./project/project.module').then((m) => m.ProjectPageModule),
   },
   {
     path: 'member',
-    loadChildren: () => import('./member/member.module').then( m => m.MemberPageModule)
+    loadChildren: () =>
+      import('./member/member.module').then((m) => m.MemberPageModule),
+  },
+  {
+    path: 'member-detail/:id',
+    loadChildren: () =>
+      import('./member-detail/member-detail.module').then(
+        (m) => m.MemberDetailPageModule
+      ),
   },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
