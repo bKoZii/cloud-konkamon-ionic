fddc285e609813069f443e7510af3cb0942f755f
Konkamon Sion <bigkonkamon@gmail.com>
2024-04-04 19:28:42 +0700

    "EditPage" Route

--- src/app/member/member-routing.module.ts
+++ src/app/member/member-routing.module.ts
@@ -6,8 +6,19 @@ import { MemberPage } from './member.page';
 const routes: Routes = [
   {
     path: '',
-    component: MemberPage
-  }
+    component: MemberPage,
+  },
+  {
+    path: 'edit-page',
+    loadChildren: () => import('./edit-page/edit-page.module').then( m => m.EditPagePageModule)
+  },
+  // {
+  //   path: 'member-detail/:id',
+  //   loadChildren: () =>
+  //     import('../member-detail/member-detail.module').then(
+  //       (m) => m.MemberDetailPageModule
+  //     ),
+  // },
 ];
 
 @NgModule({
